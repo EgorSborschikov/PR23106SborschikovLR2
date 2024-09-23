628b6d55539a63c303d9b6889025e8e6288df59e
egor <egorsborsikov250575@gmail.com>
2024-09-23 21:22:56 +0700

    update

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,20 +1,42 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 
-namespace labwork2;
-
-public partial class MainWindow : Window
+namespace labwork2
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
-    }
-    private void MenuButton_Click(object sender, RoutedEventArgs e)
-    {
-    if (MenuButton.ContextMenu != null)
+        public MainWindow()
         {
-            MenuButton.ContextMenu.Open(MenuButton);
+            InitializeComponent();
+        }
+        private void MenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MenuButton.ContextMenu != null)
+            {
+                MenuButton.ContextMenu.Open(MenuButton);
+            }
+        }
+
+        private void Problem1_Click(object sender, RoutedEventArgs e)
+        {
+            ModuleNumbers window = new ModuleNumbers();
+            window.Show();
+        }
+        private void Problem2_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private void Problem3_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private void Problem4_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private void Problem5_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
-
 }
---
+++ ModuleNumbers.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace labwork2
+{
+    public partial class ModuleNumbers : Window
+    {
+        public ModuleNumbers()
+        {
+            InitializeComponent();
+        }
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
